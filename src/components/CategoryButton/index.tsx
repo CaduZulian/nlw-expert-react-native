6fa60850147aea16b59b389ac2d9527e153675b0
Carlos Eduardo Teixeira Zulian <71992281+CaduZulian@users.noreply.github.com>
2024-02-07 09:47:39 -0300

    feat: primeira aula

---
+++ src/components/CategoryButton/index.tsx
@@ -0,0 +1,21 @@
+import { Pressable, Text } from "react-native";
+import { CategoryButtonProps } from "./models";
+import clsx from "clsx";
+
+export const CategoryButton = ({
+  title,
+  isSelected,
+  ...rest
+}: CategoryButtonProps) => {
+  return (
+    <Pressable
+      className={clsx(
+        "bg-slate-800 px-4 justify-center rounded-md h-10",
+        isSelected && "border-2 border-lime-300",
+      )}
+      {...rest}
+    >
+      <Text className="text-slate-100 font-subtitle text-sm">{title}</Text>
+    </Pressable>
+  );
+};
